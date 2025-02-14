abdb6f52b7e37e83e1d14e7345d106b9c0545120
MicaHorton <38822211+MicaHorton@users.noreply.github.com>
2025-02-13 22:32:09 -0800

    Allow user to select video on physical iPhone

---
+++ App.tsx
@@ -0,0 +1,129 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+import type {PropsWithChildren} from 'react';
+import {
+    Button,
+    SafeAreaView,
+    ScrollView,
+    StatusBar,
+    StyleSheet,
+    Text,
+    useColorScheme,
+    View,
+} from 'react-native';
+
+import {
+    Colors,
+    DebugInstructions,
+    Header,
+    LearnMoreLinks,
+    ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+
+import {Asset, launchImageLibrary} from 'react-native-image-picker';
+
+type SectionProps = PropsWithChildren<{
+    title: string;
+}>;
+
+function Section({children, title}: SectionProps): React.JSX.Element {
+    const isDarkMode = useColorScheme() === 'dark';
+    return (
+        <View style={styles.sectionContainer}>
+            <Text
+                style={[
+                    styles.sectionTitle,
+                    {
+                        color: isDarkMode ? Colors.white : Colors.black,
+                    },
+                ]}>
+                {title}
+            </Text>
+            <Text
+                style={[
+                    styles.sectionDescription,
+                    {
+                        color: isDarkMode ? Colors.light : Colors.dark,
+                    },
+                ]}>
+                {children}
+            </Text>
+        </View>
+    );
+}
+
+function App(): React.JSX.Element {
+    const isDarkMode = useColorScheme() === 'dark';
+
+    const backgroundStyle = {
+        backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+    };
+
+    const handleSelectVideo = async () => {
+        console.log('Selecting video...');
+
+        await launchImageLibrary({mediaType: 'video'}).then(response => {
+            console.log('Response:', response);
+            if (response.didCancel) {
+                console.log('User cancelled video picker');
+            } else if (response.errorMessage) {
+                console.log('Error:', response.errorMessage);
+            } else {
+                if (response.assets && response.assets.length > 0) {
+                    const selectedVideo = response.assets[0];
+                    setVideo(selectedVideo);
+                }
+            }
+        });
+    };
+
+    return (
+        <SafeAreaView style={backgroundStyle}>
+            <StatusBar
+                barStyle={isDarkMode ? 'light-content' : 'dark-content'}
+                backgroundColor={backgroundStyle.backgroundColor}
+            />
+            <ScrollView
+                contentInsetAdjustmentBehavior="automatic"
+                style={backgroundStyle}>
+                <Header />
+                <View
+                    style={{
+                        backgroundColor: isDarkMode
+                            ? Colors.black
+                            : Colors.white,
+                    }}>
+                    <Section title="Step One">Hello world!</Section>
+                    <Button title="Select Video" onPress={handleSelectVideo} />
+                </View>
+            </ScrollView>
+        </SafeAreaView>
+    );
+}
+
+const styles = StyleSheet.create({
+    sectionContainer: {
+        marginTop: 32,
+        paddingHorizontal: 24,
+    },
+    sectionTitle: {
+        fontSize: 24,
+        fontWeight: '600',
+    },
+    sectionDescription: {
+        marginTop: 8,
+        fontSize: 18,
+        fontWeight: '400',
+    },
+    highlight: {
+        fontWeight: '700',
+    },
+});
+
+export default App;
